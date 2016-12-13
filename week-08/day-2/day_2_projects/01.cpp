e14a206df390f53f2ef62619f11f57671503d73b
korompaidani <korompaidani@gmail.com>
2016-12-13 12:30:25 +0100

    first proj

---
+++ week-08/day-2/day_2_projects/01.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm> 
+
+using namespace std;
+
+void print_content(int i) {
+  cout << i << endl;
+}
+
+int main() {
+  vector<int> v;
+  for (int i = 0; i < 10; ++i) {
+    v.push_back(i);
+  }
+
+  for (vector<int>::iterator it = v.begin(); it < v.begin() + 5; it++) {
+    cout << *it << endl;
+  }
+  ///fuck yeah!!! :)
+  for_each(v.begin(), v.begin() + 5, print_content);
+  // Print the first 5 elements of the vector using iterators
+  system("pause");
+
+  return 0;
+}
