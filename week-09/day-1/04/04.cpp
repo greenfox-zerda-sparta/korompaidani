efcfb74c9025f17b3ed33f75a5fabe49c410e9be
korompaidani <korompaidani@gmail.com>
2016-12-19 17:08:50 +0100

    test and main in same file with macro

--- week-09/day-1/04/04.cpp
+++ week-09/day-1/04/04.cpp
@@ -1,9 +1,33 @@
 #include "stdafx.h"
-#include "WordToolbox.hpp"
+#define CATCH_CONFIG_MAIN
 #include <iostream>
+#include "WordToolbox.hpp"
 
-int main4() {
-
+#ifdef CATCH_CONFIG_MAIN
+int main() {
+  WordToolbox wtb("Rogue One");
+  std::cout << wtb.get_stringHeld() << std::endl;
   return 0;
 }
+#endif
 
+#ifndef CATCH_CONFIG_MAIN
+#include "C:\Users\korom\OneDrive\Programming\TEST\catch.hpp"
+TEST_CASE("setter and getter test") {
+  WordToolbox wtb("Rogue One");
+
+  CHECK(wtb.get_stringHeld() == "Rogue One");
+  CHECK(wtb.isAnAnagram("Rugoe ONe") == true);
+  CHECK(wtb.countHowMany('o') == 2);
+}
+
+TEST_CASE("Second test") {
+  WordToolbox wSource("Tarzan's toenails");
+  std::cout << "countHowMany(\"a\")=" << wSource.countHowMany('a') << std::endl; // countHowMany("a")=3
+  std::cout << "countHowMany(\"T\")=" << wSource.countHowMany('T') << std::endl; // countHowMany("T")=2
+  std::cout << "countHowMany(\"t\")=" << wSource.countHowMany('t') << std::endl; // countHowMany("t")=2
+  std::cout << "countHowMany(\"x\")=" << wSource.countHowMany('x') << std::endl; // countHowMany("x")=0
+  std::cout << "countHowMany(\" \")=" << wSource.countHowMany(' ') << std::endl; // countHowMany(" ")=1
+  CHECK(wSource.countHowMany('a') == 3);
+}
+#endif
