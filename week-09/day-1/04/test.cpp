44a0e7fcd2aeeed9fbe09f633e9423e818c82b93
korompaidani <korompaidani@gmail.com>
2016-12-19 16:56:09 +0100

    test cases

--- week-09/day-1/04/test.cpp
+++ week-09/day-1/04/test.cpp
@@ -12,9 +12,11 @@ TEST_CASE("setter and getter test") {
 }
 
 TEST_CASE("Second test") {
-  WordToolbox wtb("Rogue One");
-
-  CHECK(wtb.get_stringHeld() == "Rogue One");
-  CHECK(wtb.isAnAnagram("Rugoe ONe") == true);
-  CHECK(wtb.countHowMany('o') == 2);
+  WordToolbox wSource("Tarzan's toenails");
+  std::cout << "countHowMany(\"a\")=" << wSource.countHowMany('a') << std::endl; // countHowMany("a")=3
+  std::cout << "countHowMany(\"T\")=" << wSource.countHowMany('T') << std::endl; // countHowMany("T")=2
+  std::cout << "countHowMany(\"t\")=" << wSource.countHowMany('t') << std::endl; // countHowMany("t")=2
+  std::cout << "countHowMany(\"x\")=" << wSource.countHowMany('x') << std::endl; // countHowMany("x")=0
+  std::cout << "countHowMany(\" \")=" << wSource.countHowMany(' ') << std::endl; // countHowMany(" ")=1
+  CHECK(wSource.countHowMany('a') == 2);
 }
