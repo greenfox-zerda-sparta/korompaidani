b8c2ec9c67fbff7a1bc334433174b4631c2dac13
korompaidani <korompaidani@gmail.com>
2016-12-27 20:59:08 +0100

    my code is cleaner than my room

--- week-09/day-3/Gomoku/my_game.cpp
+++ week-09/day-3/Gomoku/my_game.cpp
@@ -1,30 +1,37 @@
 #include "my_game.hpp"
 
-Map my_map(19);
-Player player_1("a");
-Player player_2("b", player_1);
-SDL_Window sdl_window(570, 570, 30);
+//Map my_map(19);
+//Player player_1("a");
+//Player player_2("b", player_1);
+//SDL_Window sdl_window(570, 570, 30);
 
 my_game::my_game(std::string player_1_name, std::string player_2_name, unsigned short int screen_size, unsigned short int map_size) {
+  this->my_map = new Map(map_size);
+  this->player_1 = new Player(player_1_name);
+  this->player_2 = new Player(player_2_name, *player_1);
+  this->sdl_window = new SDL_Window(screen_size, screen_size, screen_size / map_size);
   this->player_1_name = player_1_name;
   this->player_2_name = player_2_name;
   this->screen_size = screen_size;
   this->map_size = map_size;
 }
 
+my_game::~my_game() {
+}
+
 void my_game::game_handler() {
-  sdl_window.game_logic(my_map, player_2);
+  sdl_window->game_logic(*my_map, *player_1);
 }
 
 void my_game::launch() {
-  sdl_window.run(my_map, player_2);
+  sdl_window->run(*my_map, *player_1);
   print_map_vector();
 }
 
 void my_game::print_map_vector() {
-  for (int i = 0; i < my_map.get_map().size(); i++) {
-    for (int j = 0; j < my_map.get_map()[i].size(); j++) {
-      std::cout << my_map.get_map()[i][j] << ' ';
+  for (int i = 0; i < my_map->get_map().size(); i++) {
+    for (int j = 0; j < my_map->get_map()[i].size(); j++) {
+      std::cout << my_map->get_map()[i][j] << ' ';
     }
     std::cout << std::endl;
   }
--- week-09/day-3/Gomoku/my_game.hpp
+++ week-09/day-3/Gomoku/my_game.hpp
@@ -7,12 +7,17 @@
 
 class my_game {
 private:
+  Map* my_map;
+  Player* player_1;
+  Player* player_2;
+  SDL_Window* sdl_window;
   std::string player_1_name;
   std::string player_2_name;
   unsigned short int screen_size;
   unsigned short int map_size;
 public:
   my_game(std::string, std::string, unsigned short int, unsigned short int);
+  ~my_game();
   void game_handler();
   void launch();
   void print_map_vector();
