38170781e0a9163f285f18bcf339428f79579108
korompaidani <korompaidani@gmail.com>
2017-01-23 21:28:41 +0100

    the the rendered picture order is tested and it is working

--- week-13/day-1/SDL_map_generation/main.cpp
+++ week-13/day-1/SDL_map_generation/main.cpp
@@ -40,6 +40,7 @@ int main(int argc, char* argv[]) {
   std::string pic2 = "pics/academy.bmp";
   std::string pic3 = "pics/tower.bmp";
   std::string pic4 = "pics/building.bmp";
+  std::string yoda = "pics/yoda.bmp";
   
   SDL_Surface* result = SDL_LoadBMP(pic1.c_str());
   SDL_SetColorKey(result, SDL_TRUE, SDL_MapRGB(SDL_AllocFormat(SDL_GetWindowPixelFormat(window)), 0xFF, 0, 0xFF));
@@ -60,6 +61,11 @@ int main(int argc, char* argv[]) {
   SDL_SetColorKey(result4, SDL_TRUE, SDL_MapRGB(SDL_AllocFormat(SDL_GetWindowPixelFormat(window)), 0xFF, 0, 0xFF));
   pictures[pic4] = SDL_CreateTextureFromSurface(renderer, result4);
   SDL_FreeSurface(result4);
+
+  SDL_Surface* result5 = SDL_LoadBMP(yoda.c_str());
+  SDL_SetColorKey(result5, SDL_TRUE, SDL_MapRGB(SDL_AllocFormat(SDL_GetWindowPixelFormat(window)), 0xFF, 0, 0xFF));
+  pictures[yoda] = SDL_CreateTextureFromSurface(renderer, result5);
+  SDL_FreeSurface(result5);
 
   SDL_Rect temp;
   SDL_Rect temp2;
@@ -86,56 +92,115 @@ int main(int argc, char* argv[]) {
   }
 
 
-  for (int i = 0; i < my_map.size(); i++) {
-    for (int j = 0; j < my_map[i].size(); j++) {
-      if (my_map[i][j] == 0) {
 
-        temp.x = j * 64;
-        temp.y = i * 64;
-        temp.w = 64;
-        temp.h = 64;
-        SDL_RenderCopy(renderer, pictures[pic1], NULL, &temp);
-        SDL_RenderPresent(renderer);
+  int yoda_pos_x = 64;
+  int yoda_pos_y = 64;
+  int yoda_h = 64;
+  int yoda_w = 64;
 
-      }
-      else if (my_map[i][j] == 1) {
-        
-        temp.x = j * 64;
-        temp.y = i * 64;
-        temp.w = 64;
-        temp.h = 64;
-        SDL_RenderCopy(renderer, pictures[pic2], NULL, &temp);
-        SDL_RenderPresent(renderer);
+  int building_pos_x = 64;
+  int building_pos_y = 64;
+  int building_h = 128;
+  int building_w = 128;
 
-      }
-      else if (my_map[i][j] == 2) {
-
-        temp.x = j * 64;
-        temp.y = i * 64;
-        temp.w = 64;
-        temp.h = 64;
-        SDL_RenderCopy(renderer, pictures[pic3], NULL, &temp);
-        SDL_RenderPresent(renderer);
-
-      }
-    }
-  }
- 
   bool running = true;
   while (running) {
-    if (SDL_PollEvent(&event) != 0) {
-      if (event.type == SDL_QUIT) {
+
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
         running = false;
+        break;
+      case SDL_KEYDOWN:
+        switch (event.key.keysym.sym) {
+        case SDLK_RIGHT:
+          yoda_pos_x+=10;
+          break;
+        case SDLK_LEFT:
+          yoda_pos_x-=10;
+          break;
+        case SDLK_UP:
+          yoda_pos_y-=10;
+          break;
+        case SDLK_DOWN:
+          yoda_pos_y+=10;
+          break;
+        case SDLK_ESCAPE:
+          running = false;
+          break;
+        }
         break;
       }
     }
+
+    for (int i = 0; i < my_map.size(); i++) {
+      for (int j = 0; j < my_map[i].size(); j++) {
+        if (my_map[i][j] == 0) {
+
+          temp.x = j * 64;
+          temp.y = i * 64;
+          temp.w = 64;
+          temp.h = 64;
+          SDL_RenderCopy(renderer, pictures[pic1], NULL, &temp);
+          ///  SDL_RenderPresent(renderer);
+
+        }
+        else if (my_map[i][j] == 1) {
+
+          temp.x = j * 64;
+          temp.y = i * 64;
+          temp.w = 64;
+          temp.h = 64;
+          SDL_RenderCopy(renderer, pictures[pic2], NULL, &temp);
+          /// SDL_RenderPresent(renderer);
+
+        }
+        else if (my_map[i][j] == 2) {
+
+          temp.x = j * 64;
+          temp.y = i * 64;
+          temp.w = 64;
+          temp.h = 64;
+          SDL_RenderCopy(renderer, pictures[pic3], NULL, &temp);
+          ///SDL_RenderPresent(renderer);
+
+        }
+      }
+    }
+
+    if (yoda_pos_y < building_pos_y + (float)building_h * 0.2) {
+      temp2.x = yoda_pos_x;
+      temp2.y = yoda_pos_y;
+      temp2.w = yoda_w;
+      temp2.h = yoda_h;
+      SDL_RenderCopy(renderer, pictures[yoda], NULL, &temp2);
     
-    temp2.x = 64;
-    temp2.y = 64;
-    temp2.w = 128;
-    temp2.h = 128;
-    SDL_RenderCopy(renderer, pictures[pic4], NULL, &temp2);
+      temp.x = building_pos_x;
+      temp.y = building_pos_y;
+      temp.w = building_w;
+      temp.h = building_h;
+      SDL_RenderCopy(renderer, pictures[pic4], NULL, &temp);
+
+
+    }
+    else {
+      temp.x = 64;
+      temp.y = 64;
+      temp.w = 128;
+      temp.h = 128;
+      SDL_RenderCopy(renderer, pictures[pic4], NULL, &temp);
+
+
+      temp2.x = yoda_pos_x;
+      temp2.y = yoda_pos_y;
+      temp2.w = yoda_w;
+      temp2.h = yoda_h;
+      SDL_RenderCopy(renderer, pictures[yoda], NULL, &temp2);
+
+    }
+
     SDL_RenderPresent(renderer);
+    
     
   }
 
@@ -143,6 +208,9 @@ int main(int argc, char* argv[]) {
   SDL_DestroyTexture(pictures[pic2]);
   SDL_DestroyTexture(pictures[pic3]);
   SDL_DestroyTexture(pictures[pic4]);
+  SDL_DestroyTexture(pictures[yoda]);
+
+  SDL_RenderClear(renderer);
   SDL_DestroyWindow(window);
   
   SDL_QUIT;
